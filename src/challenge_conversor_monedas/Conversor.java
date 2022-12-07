492854654f9e7e531ee65e63866985cb2005c1ff
Rodrigo Valle <fernandovalle939@gmail.com>
2022-12-07 15:46:57 -0600

    Iniciando proyecto e importando librería javax.swing

---
+++ src/challenge_conversor_monedas/Conversor.java
@@ -0,0 +1,20 @@
+package challenge_conversor_monedas;
+import javax.swing.*;
+
+public class Conversor {
+
+	public static void main(String[] args) {
+	
+		
+		int a =0;
+		int b =0;
+		
+		do {
+			
+			
+			
+		} while(a<b);
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ferna
+ *
+ */
+module challenge_conversor_monedas {
+}
